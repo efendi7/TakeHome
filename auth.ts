fc1559bb625664bad6641f5ec17660062f4e24b3
Muhammad Ma'mun Efendi <muhammadefendi002@gmail.com>
2025-09-23 15:43:45 +0700

    fix auth route

--- auth.ts
+++ auth.ts
@@ -19,7 +19,12 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { 
+  handlers: { GET, POST }, // Tambahkan baris ini
+  auth, 
+  signIn, 
+  signOut 
+} = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
